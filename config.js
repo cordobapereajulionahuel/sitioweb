0a0ae78c0d77e55228ddaa0a5b406a333b513fe5
cordobapereajulionahuel <cordobapereajulionahuel@gmail.com>
2022-11-11 16:41:12 -0300

    se añadio nuevas configuraciones js,css y html

--- config.js
+++ config.js
@@ -1,10 +1,18 @@
-let img=document.querySelector(".imgone");
-img.addEventListener("click", mostrar);
-function mostrar(){
+let a=document.querySelector(".a");
+a.addEventListener("click",rojo);
+ function rojo(){
     let p=document.querySelector(".p");
    p.style.color="red";
-
-
-
-    
+}
+let b=document.querySelector(".b");
+b.addEventListener("click", verde);
+function verde(){
+    let p=document.querySelector(".p");
+   p.style.color="rgb(113, 224, 113)";
+}
+let c=document.querySelector(".c");
+c.addEventListener("click", black);
+function black(){
+    let p=document.querySelector(".p");
+   p.style.color="black";
 }
