237209016db057d3b383f9109d986b987dbd4b38
cordobapereajulionahuel <cordobapereajulionahuel@gmail.com>
2022-11-10 22:46:32 -0300

    se añadio configuraciones js

---
+++ config.js
@@ -0,0 +1,10 @@
+let img=document.querySelector(".imgone");
+img.addEventListener("click", mostrar);
+function mostrar(){
+    let p=document.querySelector(".p");
+   p.style.color="red";
+
+
+
+    
+}
